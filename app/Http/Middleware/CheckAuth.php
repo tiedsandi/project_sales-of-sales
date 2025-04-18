af89fb83a5b282bd46a679d09b3a676224a2f554
tiedsandi <fachransandi@gmail.com>
2025-04-19 05:57:28 +0700

    add CheckAuth middleware to enforce authentication on dashboard route

---
+++ app/Http/Middleware/CheckAuth.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+use RealRashid\SweetAlert\Facades\Alert;
+
+class CheckAuth
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (!Auth::check()) {
+            Alert::warning('Warning Title', 'Harus Login');
+            return redirect('/');
+        }
+
+        return $next($request);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,9 @@ Route::get('/', [AuthController::class, 'login'])->name('login');
 Route::post('action-login', [AuthController::class, 'actionLogin']);
 Route::get('logout', [AuthController::class, 'logout']);
 
-Route::get('/dashboard', function () {
-  return view('dashboard');
+
+Route::group(['middleware' => 'checkAuth'], function () {
+  Route::get('/dashboard', function () {
+    return view('dashboard');
+  });
 });
