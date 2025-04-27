19d114251ba37a5cdd5d895ce64537ad8e9b9b77
tiedsandi <fachransandi@gmail.com>
2025-04-27 09:24:28 +0700

    pos-final, adding migration User, Role, Category, Product, Order, Order Detail

---
+++ pos-final/app/Models/Order.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    protected $fillable = [
+        'order_code',
+        'order_date',
+        'order_amount',
+        'order_change',
+        'order_status'
+    ];
+}
---
+++ pos-final/app/Models/Product.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+
+    use HasFactory;
+
+    protected $fillable = [
+        'category_id',
+        'product_name',
+        'product_photo',
+        'product_qty',
+        'product_price',
+        'product_description',
+        'is_active'
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function scopeActive($query)
+    {
+        return $query->where('is_active', true);
+    }
+}
